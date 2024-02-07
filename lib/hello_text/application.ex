a6eb905b25047eca709b52e863b9b1053351f1e2
Jia Wang <wj2016@gmail.com>
2024-02-07 14:52:54 +0100

    load the model and run serving

--- lib/hello_text/application.ex
+++ lib/hello_text/application.ex
@@ -7,9 +7,20 @@ defmodule HelloText.Application do
 
   @impl true
   def start(_type, _args) do
+    {:ok, model_info} =
+      Bumblebee.load_model({:hf, "finiteautomata/bertweet-base-emotion-analysis"})
+
+    {:ok, tokenizer} = Bumblebee.load_tokenizer({:hf, "vinai/bertweet-base"})
+
+    serving =
+      Bumblebee.Text.text_classification(model_info, tokenizer,
+        top_k: 1,
+        compile: [batch_size: 4, sequence_length: 100],
+        defn_options: [compiler: EXLA]
+      )
+
     children = [
-      # Starts a worker by calling: HelloText.Worker.start_link(arg)
-      # {HelloText.Worker, arg}
+      {Nx.Serving, serving: serving, name: MyTextClassification.Serving, batch_timeout: 500}
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
